085e685b45c58c05bcb78cc0b1a6315bffd5292a
VicenteAguileraPerez <vicente_prez@hotmail.com>
2020-06-25 16:49:12 -0500

    [ADD] 04.TipoDeDatoCaracter.kt

---
+++ src/04.TipoDeDatoCaracter.kt
@@ -0,0 +1,18 @@
+
+fun main(args: Array<String>)
+{
+    var char1:Char
+    char1='1'
+    println(char1)
+    //Java-> char x=49;
+    var char2:Char = 49.toChar()
+    println(char2)
+
+    var char3:Char = '\n'
+    println("hola"+char3+"mundo")
+
+    var char4:Char = '\u0047'//'\uXXXX'
+    println(char4)
+
+
+}
