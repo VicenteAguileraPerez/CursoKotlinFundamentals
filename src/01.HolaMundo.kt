c1b5b7e35bb36380136dc6ed9f8ee7a7fa926c00
Vicente Aguilera Pérez <vicente_prez@hotmail.com>
2020-08-01 11:02:06 -0500

    [ADD] 07.LiteralConstants.kt

--- src/01.HolaMundo.kt
+++ src/01.HolaMundo.kt
@@ -5,5 +5,6 @@
 fun main(args: Array<String>) {
     print("Mensaje sin salto de línea \n")
     println("Mensaje con salto de línea")
+   
 
 }
---
+++ src/07.LiteralConstants.kt
@@ -0,0 +1,22 @@
+fun main()
+{
+    //literal constants enteras
+    var long:Long = 123
+    println(long)
+
+    var hexa:Int = 0xFF
+    println(hexa)
+
+    var bin:Int = 0b00001111;
+    println(bin)
+
+    //literal constants enteras
+
+    var float:Float = 12.35F
+    println(float)
+
+
+    var expo:Double = 123e-6;//123x10^-6
+    println(expo)
+
+}
