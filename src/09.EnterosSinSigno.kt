03b390b284c78c1d537ba8da65be9fd5ab38e19c
Vicente Aguilera Pérez <vicente_prez@hotmail.com>
2020-08-02 18:08:27 -0500

    [ADD] 09.EnterosSinSigno.kt

---
+++ src/09.EnterosSinSigno.kt
@@ -0,0 +1,13 @@
+fun main()
+{
+
+    var ubyte:UByte=122U
+    println(ubyte)
+    var ushort:UShort=32500u
+    println(ushort)
+    var uint: UInt = 4_000_000_000u
+    println(uint)
+    val ulong:ULong =90_000_000_000UL
+    println(ulong)
+
+}
