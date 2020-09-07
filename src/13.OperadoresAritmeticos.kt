98995d971cf3f579c318c5aacb4ffbb8d094f28f
Vicente Aguilera Pérez <vicente_prez@hotmail.com>
2020-09-07 18:57:25 -0500

    [ADD] 13.OperadoresAritmeticos.kt

---
+++ src/13.OperadoresAritmeticos.kt
@@ -0,0 +1,14 @@
+fun main()
+{
+    var num1=10
+    var num2=20
+
+    println(num1+num2)
+    println(num2-num1)
+    println(num1*num2)
+    println(num2/num1)
+    //num1=num1+1
+    print(num2%num1) //20/11=1   9
+
+
+}
