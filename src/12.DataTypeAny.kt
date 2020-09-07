af08942e2c62f90085bb7dcd4453f061d111edb7
Vicente Aguilera Pérez <vicente_prez@hotmail.com>
2020-09-07 18:55:40 -0500

    [ADD] 12.DataTypeAny.kt

---
+++ src/12.DataTypeAny.kt
@@ -0,0 +1,16 @@
+
+fun main()
+{
+    var cualquiera:Any=10
+    println(cualquiera)
+    var int:String="Hola"
+    println(int)
+    cualquiera = true
+    println(cualquiera)
+    cualquiera = 12.5
+    println(cualquiera)
+    cualquiera = "Hola"
+    println(cualquiera)
+    println(int.equals(cualquiera))
+
+}
